7a550896ea257064438523803d6d2521c4460622
haik-hnl <contact@haik-henkel.de>
2023-10-04 15:12:56 +0200

    1st push

---
+++ src/components/Component.vue
@@ -0,0 +1,31 @@
+<template></template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const questions = ref([
+  {
+    question: "Warum ist Aki so ein Huen?",
+    answer: 0,
+    options: ["Schlecht in LoL", "Arsch in Rl", "No aim", "Kleine Banane"],
+    selected: null,
+  },
+  {
+    question: "Warum ist Aki so ein Huen?",
+    answer: 0,
+    options: ["Schlecht in LoL", "Arsch in Rl", "No aim", "Kleine Banane"],
+    selected: null,
+  },
+  {
+    question: "Warum ist Aki so ein Huen?",
+    answer: 0,
+    options: ["Schlecht in LoL", "Arsch in Rl", "No aim", "Kleine Banane"],
+    selected: null,
+  },
+]);
+
+const quizCompleted = ref(false);
+const currentQuestion = ref(0);
+const score = computed(() => {});
+</script>
+<style scoped></style>
---
+++ src/components/halhal.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup lang="ts">
+let isHaikEinLoser: boolean = true;
+
+if ((isHaikEinLoser = true)) {
+  console.log("Spass, Haik ist so cool!");
+} else {
+  console.log("Haik ist immernoch cool! B)");
+}
+</script>
+<style scoped></style>
