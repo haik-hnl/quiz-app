2de0a935eb397a7c049af4494fbc7c43bc201d33
root <root@DESKTOP-9KP9O8T>
2023-10-06 17:07:05 +0200

    added array and loop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,38 @@
-<template></template>
+<template>
+  <div>
+    <h1> {{ currentQuestion.question }} </h1>
+    <ul>
+      <li v-for="option in currentQuestion.options" :key="options.id">
+        <button @click=""> {{  }} </button>
+      </li>
+    </ul>
+  </div>
+</template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import Question from "../module/Question";
 
-const questions = ref([
+const questions: Question[] = [
   {
-    question: "Test",
-    answer: 0,
-    options: ["AntwortA", "AntwortB", "AntwortC"],
-    selected: null,
+    id: 1,
+    question: "test",
+    options: [
+      { id: 1, text: "option1", isCorrect: true },
+      { id: 2, text: "option2", isCorrect: false },
+      { id: 3, text: "option3", isCorrect: false }
+    ]
   },
-  {
-    question: "Test",
-    answer: 0,
-    options: ["AntwortA", "AntwortB", "AntwortC"],
-    selected: null,
-  },
-  {
-    question: "Test",
-    answer: 0,
-    options: ["AntwortA", "AntwortB", "AntwortC"],
-    selected: null,
-  },
-]);
+];
 
+const allQuestions = questions.length
+const currentQuestion = ref(questions[0]);
+const currentIndex = ref(0);
+
+const selectQuestion = () => {
+  if (currentIndex.value < allQuestions - 1) {
+    currentIndex.value ++;
+    currentQuestion.value = questions[currentIndex.value];
+  }
+};
 </script>
